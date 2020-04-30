7be15a46fff7b57d1ccfc2b8fd12649f2712b1ec
Gambito Code/ <asesor.pedro@gmail.com>
2020-04-29 21:28:24 -0500

    migraciones
    modifique las migraciones para que se pueda emitir el metodo migrate en vez del metodo migrate:fress y de esa forma no perder olos datos de produccion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,49 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                if (!Schema::hasColumn('users', 'id')) {
+                    $table->id();
+                }
+                if (!Schema::hasColumn('users', 'name')) {
+                    $table->string('name');
+                }
+                if (!Schema::hasColumn('users', 'email')) {
+                    $table->string('email')->unique();
+                }
+                if (!Schema::hasColumn('users', 'username')) {
+                    $table->string('username')->nullable()->unique();
+                }
+                if (!Schema::hasColumn('users', 'email_verified_at')) {
+                    $table->timestamp('email_verified_at')->nullable();
+                }
+                if (!Schema::hasColumn('users', 'password')) {
+                    $table->string('password');
+                }
+                if (!Schema::hasColumn('users', 'remember_token')) {
+                    $table->rememberToken();
+                }
+                if (!Schema::hasColumn('users', 'created_at')) {
+                    $table->timestamps();
+                }
+                if (!Schema::hasColumn('users', 'deleted_at')) {
+                    $table->softDeletes();
+                }
+            });
+        }else{
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email')->unique();
+                $table->string('username')->nullable()->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->rememberToken();
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
     }
 
     /**
