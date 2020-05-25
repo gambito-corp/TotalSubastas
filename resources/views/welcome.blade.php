77ad269a211d611427d64bafd14c83f7c4055d13
i820189 <javier5abril@gmail.com>
2020-05-25 11:56:06 -0500

    titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Total Subastas API
                 </div>
 
                 <div class="links">
