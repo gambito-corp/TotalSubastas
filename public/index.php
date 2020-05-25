b1190c37f72f7a64f8f096ff886b091ca950f4b4
i820189 <javier5abril@gmail.com>
2020-05-25 00:14:59 -0500

    de http a https

--- public/index.php
+++ public/index.php
@@ -7,6 +7,14 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+if((isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] === 'http')){
+    // redireccionar a https
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+
 define('LARAVEL_START', microtime(true));
 
 /*
