0b987cae4f0a862b5537debd98b2b82eb4caee58
i820189 <javier5abril@gmail.com>
2020-05-24 23:13:56 -0500

    forzar https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        if(config( key: 'app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+        
     }
 }
