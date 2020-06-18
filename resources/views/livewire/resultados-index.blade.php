64af1262439f79b48593f21d27d249869a99bca0
Gambito Code/ <asesor.pedro@gmail.com>
2020-06-18 06:36:49 -0500

    slide de resultados creado, en la tarde doy funcionabilidad a los filtros de busqueda

--- resources/views/livewire/resultados-index.blade.php
+++ resources/views/livewire/resultados-index.blade.php
@@ -4,13 +4,16 @@
     <div class="clearfix"></div>
     <hr>
     <!--Carousel Wrapper-->
+    @php
+        $productos = App\Producto::all();
+            $users = App\User::all();
+    @endphp
     <div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
 
         <!--Controls-->
         <div class="controls-top">
             <a class="btn-floating" href="#multi-item-example" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
-            <a class="btn-floating" href="#multi-item-example" data-slide="next"><i
-                    class="fas fa-chevron-right"></i></a>
+            <a class="btn-floating" href="#multi-item-example" data-slide="next"><i class="fas fa-chevron-right"></i></a>
         </div>
         <!--/.Controls-->
 
@@ -18,116 +21,37 @@
         <ol class="carousel-indicators">
             <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
             <li data-target="#multi-item-example" data-slide-to="1"></li>
-
         </ol>
         <!--/.Indicators-->
 
         <!--Slides-->
         <div class="carousel-inner" role="listbox">
-
-            <!--First slide-->
-            <div class="carousel-item active">
-                <div class="col-md-4" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="{{asset('img/vehiculos/coche.png')}}" alt="Imagen del Vehiculo">
-                        <div class="card-body">
-                            <h4 class="card-title text-success">subasta</h4>
-                            <p class="card-text">Chevrolet A91/2018</p>
-                            <a class="btn btn-success">$ 3500 <i class="fa fa-long-arrow-right"></i></a>
+        @foreach ($productos->chunk(3) as $key => $productCollection)
+            <div class="row">
+                <!--First slide-->
+                <div class="carousel-item {{$key == 0 ? 'active' : '' }}">
+                    @foreach ($productCollection as $product)
+                        <div class="col-md-4" style="float:left">
+                            <div class="card mb-2">
+                                <img class="card-img-top" src="{{asset('img/vehiculos/coche.png')}}" alt="Imagen del Vehiculo">
+                                <div class="card-body">
+                                    <h4 class="card-title text-success">subasta</h4>
+                                    <p class="card-text">Chevrolet A91/2018</p>
+                                    <a class="btn btn-success">$ 3500 <i class="fa fa-long-arrow-right"></i></a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    @endforeach
 
-                <div class="col-md-4" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="{{asset('img/vehiculos/coche.png')}}" alt="Imagen del Vehiculo">
-                        <div class="card-body">
-                            <h4 class="card-title text-success">subasta</h4>
-                            <p class="card-text">Chevrolet A91/2018</p>
-                            <a class="btn btn-success">$ 3500 <i class="fa fa-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="{{asset('img/vehiculos/coche.png')}}" alt="Imagen del Vehiculo">
-                        <div class="card-body">
-                            <h4 class="card-title text-success">subasta</h4>
-                            <p class="card-text">Chevrolet A91/2018</p>
-                            <a class="btn btn-success">$ 3500 <i class="fa fa-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
+                <!--/.First slide-->
             </div>
-            <!--/.First slide-->
-
-            <!--Second slide-->
-            <div class="carousel-item">
-
-                <div class="col-md-3" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3" style="float:left">
-                    <div class="card mb-2">
-                        <img class="card-img-top"
-                             src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                                card's content.</p>
-                            <a class="btn btn-primary">Button</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <!--/.Second slide-->
-
-
-
+        @endforeach
         </div>
         <!--/.Slides-->
-
     </div>
     <!--/.Carousel Wrapper-->
+
+
+
+
 </div>
