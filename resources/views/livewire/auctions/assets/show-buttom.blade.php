4f9122a06fd44d275eb9ad5f6587307fc8a6427a
Gambito Code/ <asesor.pedro@gmail.com>
2020-06-29 21:23:15 -0500

    Update show-buttom.blade.php

--- resources/views/livewire/auctions/assets/show-buttom.blade.php
+++ resources/views/livewire/auctions/assets/show-buttom.blade.php
@@ -53,6 +53,8 @@
             @if($Estado[0] == 'online')
                 @auth
                     <a class="btn btn-primary rounded-pill pr-4 btn-to_action-bottom text-light" href="{{ route('auctionLiveDetail', ['id' => $Producto->id])}} "><i class="fas fa-eye pr-3 pl-3 "></i> Participar </a>
+                @else
+                    <a class="btn btn-success rounded-pill pr-4 btn-to_action-bottom text-light" href="{{ route('login')}} "><i class="fas fa-user pr-3 pl-3 "></i> Logueate </a>
                 @endauth
             @endif
             @if($Estado[0] == 'puja')
@@ -60,6 +62,8 @@
                     <form wire:submit.prevent="pujar">
                         <button class="btn btn-primary rounded-pill pr-4 btn-to_action-bottom text-light"><i class="fas fa-gavel fa-rotate-270 pr-3 pl-3 "></i> Pujar {{$Producto->puja}} $ </button>
                     </form>
+                @else
+                    <a class="btn btn-success rounded-pill pr-4 btn-to_action-bottom text-light" href="{{ route('login')}} "><i class="fas fa-user pr-3 pl-3 "></i> Logueate </a>
                 @endauth
             @endif
             @if($Estado[0] == 'Finalizada')
