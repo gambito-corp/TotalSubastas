fffecd1a59ce5de6f43a10fc75287e46f99c210d
Gambito Code/ <asesor.pedro@gmail.com>
2020-07-21 14:27:18 -0500

    Update PerfilController.php

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PerfilController extends Controller
 {
-    //
+    public function show ()
+    {
+        return view('perfil.account');
+    }
+
+    public function edit ()
+    {
+        return view('perfil.profile-edit');
+    }
 }
