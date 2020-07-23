c10c976e293fc06270e9f743d7d2ea563fac9586
Gambito Code/ <asesor.pedro@gmail.com>
2020-07-22 23:13:00 -0500

    Update account.blade.php

--- resources/views/perfil/account.blade.php
+++ resources/views/perfil/account.blade.php
@@ -17,7 +17,7 @@
                     <!-- Accordion card -->
 
                     <div class="bg-light-card shadow-sm radius">
-                        <img src="{{asset(Auth::user()->avatar)}}" class=" mt-4" with="" alt="..." style="width: 90px;
+                        <img src="{{asset(Auth::user() ? Auth::user()->avatar: 'img/user/default.png')}}" class=" mt-4" with="" alt="..." style="width: 90px;
                                     height: 90px; border-radius:50%;">
                         <div class="card-body pl-0 pr-0">
                             <h5 class="card-title font-weight-bold text-dark">{{$persona->nombres.' '.$persona->apellidos}}</h5>
