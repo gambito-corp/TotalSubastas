202846d8defaf362f71eaf0487dda48bd9c8e01a
Gambito Code/ <asesor.pedro@gmail.com>
2020-07-25 22:45:19 -0500

    upo

--- resources/views/admin/assets/aside.blade.php
+++ resources/views/admin/assets/aside.blade.php
@@ -21,7 +21,7 @@
         </div>
 
         <!-- Sidebar Menu -->
-        <nav class="mt-2">
+        <nav class="mt-2" >
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 {{-- menu-open para abrir el menu--}}
             <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
