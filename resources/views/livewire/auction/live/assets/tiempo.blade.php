ed82ddf9cc11ef54d6ea06a8c6cc53e4c540c2b4
Gambito Code/ <asesor.pedro@gmail.com>
2020-08-01 11:56:37 -0500

    Update tiempo.blade.php

--- resources/views/livewire/auction/live/assets/tiempo.blade.php
+++ resources/views/livewire/auction/live/assets/tiempo.blade.php
@@ -1,10 +1,10 @@
-<div class="col" wire:pool.100ms="$refresh">
+<div class="col" >
     @if ($producto->finalized_at->subSeconds(20)<=now())
         <p class="{{$producto->finalized_at>=now()? 'col-md-2 animate__animated animate__fadeIn': 'col-md-12 animate__animated animate__fadeIn'}}">
             {{$producto->finalized_at>=now()? 'En 00:'.$producto->finalized_at->diffInSeconds(now()): 'La Subasta finalizo, el ganador es '.$ganador }}
         </p>
         @if ($producto->finalized_at>=now())
-            <div class="col-md 10">
+            <div class="col-md 10" wire:pool="$refresh">
                 <div class="progress rounded-pill border border-light ml-2 animate__animated animate__fadeIn">
                     <div
                         class="progress-bar progress-bar-striped progress-bar-animated bg-warning"
