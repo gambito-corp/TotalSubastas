75f076f118b77ece8d5d7d364c116627ee916e07
Gambito Code/ <asesor.pedro@gmail.com>
2020-08-02 08:01:35 -0500

    update

--- resources/views/livewire/auction/live/assets/mensajes.blade.php
+++ resources/views/livewire/auction/live/assets/mensajes.blade.php
@@ -2,14 +2,14 @@
     @isset($mensajes)
         @forelse($mensajes as $value )
             @if ($value->user_id == Auth::id())
-                <div class="mt-2 alert text-light rounded-pill ancho--220 float-right chat-box-user align-items-end animate__animated animate__backInLeft" width="55%">
+                <div class="mt-2 alert text-light rounded-pill ancho--220 float-right chat-box-user align-items-end" width="55%">
                     <p class="chat-box-txt">
                         <strong>{{$value->Usuario->name}}</strong>  ofertó  <strong>$ {{$value->message}}</strong>
                     </p>
                 </div>
                 <br>
             @else
-                <div class="mt-2 alert alert-light text-dark rounded-pill ancho--220 float-left chat-box-else align-items-end animate__animated animate__backInRight" width="55%">
+                <div class="mt-2 alert alert-light text-dark rounded-pill ancho--220 float-left chat-box-else align-items-end" width="55%">
                     <p class="chat-box-txt">
                         <strong>{{$value->Usuario->name}}</strong>  oferto  <strong>$ {{$value->message}}</strong>
                     </p>
@@ -18,7 +18,7 @@
             @endif
             <br>
         @empty
-            <div class="mt-2 alert text-light rounded-pill ancho--220 float-right chat-box-user align-items-end animate__animated animate__backInLeft" width="55%">
+            <div class="mt-2 alert text-light rounded-pill ancho--220 float-right chat-box-user align-items-end" width="55%">
                 <p class="chat-box-txt">
                     <strong>Subasta Por Iniciar</strong>
                 </p>
