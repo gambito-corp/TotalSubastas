63bbfefa04e3f59f94b6ec9cd2f60ad48de791d1
Gambito Code/ <asesor.pedro@gmail.com>
2020-08-06 02:37:46 -0500

    Update Index.php

--- app/Http/Livewire/Auction/Live/Index.php
+++ app/Http/Livewire/Auction/Live/Index.php
@@ -70,7 +70,7 @@ class Index extends Component
 
     public function pujar()
     {
-        if($this->producto->finalized_at <= now()->subSeconds(10) || $this->producto->user_id != Auth::id()){
+        if($this->producto->finalized_at <= now()->subSeconds(10)){
             $mensaje = intval($this->producto->precio + $this->producto->puja);
             $usuario = Auth::id();
             event(new SubastaEvent($this->producto));
