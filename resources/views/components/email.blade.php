6ce56ae206547977f92240df6fc64414c28cb415
Gambito Code/ <asesor.pedro@gmail.com>
2020-09-15 19:55:41 -0500

    registro

--- resources/views/components/email.blade.php
+++ resources/views/components/email.blade.php
@@ -6,15 +6,14 @@
     <meta name="x-apple-disable-message-reformatting">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{$titulo}}</title>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{mix('css/app.css')}}" />
-    <link rel="stylesheet" href="{{mix('css/assets.css')}}" />
-    <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous">
     <style>
+        .align-text-bottom {
+            vertical-align: text-bottom !important;
+        }
 
+        .align-text-top {
+            vertical-align: text-top !important;
+        }
     </style>
 </head>
 <body>
