c5627b26bc8e2914f4f1f4d3e0c28fdb275602fd
Gambito Code/ <asesor.pedro@gmail.com>
2020-09-16 23:00:24 -0500

    reset Pass

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -32,9 +33,24 @@ class ResetPasswordController extends Controller
 //        return 'esto es';
     }
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
+    public function reset(Request $request)
+    {
+        $request->validate($this->rules(), $this->validationErrorMessages());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+            $this->resetPassword($user, $password);
+        }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+            ? $this->sendResetResponse($request, $response)
+            : $this->sendResetFailedResponse($request, $response);
+    }
 }
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -10,36 +10,59 @@
         <div class="col-md col-md-12 mt-5 offset-md-3 text-center">
           <div class="col-sm col-sm-12 col-lg-6 p-5 main-container  mt-5">
             <h1 class="font-weight-bold">
-             Te ayudamos esta es donde nos deve redirigir
+             Te ayudamos A Recuperar Tu Cuenta
             </h1>
-            <p>ingresa tu correo electronico y te enviamos un email</p>
-          <form method="post" action="{{route('password.request')}}">
-              @csrf
-              <input type="hidden" name="token" value="token">
-            <div class="form-group row justify-content-center">
-              <div class="col-sm-10">
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text bg-light text-light_darken" id="basic-addon1"><i class="fas fa-user"></i></i></span>
+            <p>ingresa tu correo electronico y Contraseña</p>
+
+              <form method="POST" action="{{ route('password.update') }}">
+                  @csrf
+
+                  <input type="hidden" name="token" value="{{ $token }}">
+
+                  <div class="form-group row">
+                      <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+
+                          @error('email')
+                          <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                          @enderror
+                      </div>
                   </div>
-                  <input type="text" class="form-control" placeholder="Correo electronico" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-              </div>
-            </div>
-            <div class="form-group row border-bottom mb-5 pb-5 pt-5">
-              <div class="col-sm-12">
-                <button type="submit" class="btn btn-primary rounded-pill pt-2 pb-2 pl-5 pr-5">Ingresar</button>
-              </div>
-            </div>
-            <div class="form-group row justify-content-center">
-              <div class="col-sm-6">
-              <p>Aun no tienes cuenta?</p>
-              </div>
-              <div class="col-sm-6">
-              <a href="./registro.html" class="nav-item">Registrar</a>
-              </div>
-            </div>
-          </form>
+
+                  <div class="form-group row">
+                      <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                          @error('password')
+                          <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                          @enderror
+                      </div>
+                  </div>
+
+                  <div class="form-group row">
+                      <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                      </div>
+                  </div>
+
+                  <div class="form-group row mb-0">
+                      <div class="col-md-6 offset-md-4">
+                          <button type="submit" class="btn btn-primary">
+                              Actualizar Password
+                          </button>
+                      </div>
+                  </div>
+              </form>
           </div>
         </div>
         </div>
