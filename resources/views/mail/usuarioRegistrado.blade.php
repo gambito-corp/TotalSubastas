3dd052959e0d1e40e21154c458e625646f47fc3b
Gambito Code/ <asesor.pedro@gmail.com>
2020-09-18 22:26:42 -0500

    correccion tic

--- resources/views/mail/usuarioRegistrado.blade.php
+++ resources/views/mail/usuarioRegistrado.blade.php
@@ -2,9 +2,6 @@
     <img src="https://totalsubastas.s3.us-east-2.amazonaws.com/assets/Icons/security_internet/5172989_candado_compu.png" width="136" alt="SimpleApp">
     <h2 >Felicitaciones {{$user->name}}!</h2>
     <p >Ya falta poco para crear tu cuenta</p>
-    <x-slot name="oculto">
-        <input type="hidden" name="user" value="{{$hash}}">
-    </x-slot>
 
     <x-slot name="recordatorio">
         <p>Recuerda que este correo vence en 5 minutos.</p>
