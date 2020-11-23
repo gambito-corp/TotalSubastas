4912fba0866475b6d01a1f01577b3214b82e5e1f
gambito-corp <asesor.pedro@gmail.com>
2020-11-22 21:25:30 -0500

    auto

--- resources/views/auction/assets/cajaDetalle.blade.php
+++ resources/views/auction/assets/cajaDetalle.blade.php
@@ -140,50 +140,7 @@
           </div>
         </div>
         <div class="tab-pane fade  p-4 " id="contact" role="tabpanel" aria-labelledby="contact-tab">
-          <p class="text-capitalize font-weight-bold">
-            Condiciones de Ofrecimiento
-          </p>
-          <p class="">
-            Al participar en este proceso de subasta confirmas que has leído y aceptado la última versión de
-            nuestras Condiciones y
-            Términos. El activo se vende como está y donde está. Es responsabilidad del participante verificar el
-            estado del activo.
-            Toda la información publicada es referencial. Todas las imágenes son referenciales. Sin opción a
-            visitas. El ganador
-            deberá pagar el 5.00% del monto de adjudicación o mínimo US$ 50 por concepto de comisión por uso del
-            servicio de
-            subastas, en un plazo de 2 días. Esta oferta dispone de compra inmediata, con lo cual podrá adjudicarse
-            la oferta al
-            aceptar el precio que se indica en la publicación del ofrecimiento al momento de ejecutar la acción.
-          </p>
-          <p class="text-capitalize font-weight-bold">
-            Condiciones de Compra
-          </p>
-          <p class="">
-            Forma de pago: Cuentas bancarias para depósitos y/o transferencias. El incumplimiento de cualquiera de
-            las obligaciones
-            del comprador resultara en las sanciones descritas en Condiciones y términos. Como parte del proceso de
-            adjudicación se
-            solicitan los siguientes documentos. Persona natural: DNI, Datos adicionales, Recibo de Servicio, DNI
-            Conyuge - Solo
-            casados, Declaración Jurada (PPNN). Persona Jurídica: Ficha RUC, Vigencia de poderes, Datos adicionales,
-            Recibo de
-            Servicio, DNI Representante Legal, Declaración Jurada (PPJJ). Plazo de pago del (los) activo(s) es de: 2
-            días.
-            Facturable, precio incluye IGV. Los gastos de transferencia y traslado son a cargo del comprador. Los
-            gastos de custodia
-            del ítem serán asumidos por el comprador en caso no se recoja este oportunamente. El plazo para recoger
-            el(los)
-            activo(s) es de 30 días hábiles desde que se confirma el pago de este. El miembro adjudicado podrá optar
-            por una
-            extensión de plazo de pago de 4 días hábiles, abonando una reserva del 20% del monto adjudicado. El
-            vendedor penalizará
-            con 5% del valor de adjudicación al miembro adjudicado que desista de la compra luego de haber realizado
-            el abono total
-            o parcial. Dada la coyuntura y cuarentena actual, todo plazo de firma y entrega será aplazado. Durante
-            este periodo no
-            se contarán con visitas.
-          </p>
+          {!! {!!$producto->Vehiculo != null? $producto->Vehiculo->terminos: ''!!} !!}
         </div>
       </div>
     </div>
