f36d8b7c40db5388b029068da5e2e5b7f5e7452f
gambito-corp <asesor.pedro@gmail.com>
2020-11-29 16:34:37 -0500

    correlativo

--- app/Http/Controllers/ImagenesController.php
+++ app/Http/Controllers/ImagenesController.php
@@ -59,7 +59,7 @@ class ImagenesController extends Controller
         $id = Gambito::hash($id, true);
         $data = ImagenesVehiculo::where('id', $id)->first();
         $file = Image::make(Storage::disk('s3')->get('producto/set/'.$data->imagen));
-        $watermark = Image::make(Storage::disk('s3')->get('producto/marca.png'))->opacity(30)->resize('200', '200');
+        $watermark = Image::make(Storage::disk('s3')->get('producto/marca.png'))->opacity(20)->resize('200', '200');
         $file->insert($watermark, 'center')
             ->response();
         $code = 200;
