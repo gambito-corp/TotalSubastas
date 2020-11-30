1f567d0aa180717912fc0b694bdff5d3b211f1ca
gambito-corp <asesor.pedro@gmail.com>
2020-11-30 02:59:30 -0500

    final Livend

--- resources/views/assets/alertas.blade.php
+++ resources/views/assets/alertas.blade.php
@@ -1,4 +1,7 @@
 @if (session()->has('message'))
+    @isset($message)
+        @dump($message)
+    @endisset
     <div>
         <div class="{{session('alerta')? 'alert alert-'.session('alerta'): ''}}">
             {{ session('message') }}
