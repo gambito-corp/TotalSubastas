e73f3e9236246962c6cc6cb4d85b8d00fc0b942c
gambito-corp <asesor.pedro@gmail.com>
2021-05-17 22:05:02 -0500

    cambios del acta

--- resources/views/estaticas/quienessomos.blade.php
+++ resources/views/estaticas/quienessomos.blade.php
@@ -19,25 +19,25 @@
                 <br>
                 <p class="pcontainer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptate voluptatum facilis eveniet natus eius recusandae, illo, ex deleniti accusamus deserunt fugit fuga eum non repellat, enim dolores aspernatur maiores!</p>
             </div>
-            <h2>ESTO LO PUEDO ELIMINAR</h2>
-            <div class="col mb-5 mt-5">
-                <div class="row pt-5 pb-5 comment-box">
-                    <div class="col-md-9 pl-5">
-                        <h4><strong style="line-height: 2.3rem">Gracias a totalSubastas.com pudimos vender los activos que teníamos en el almacen, nos preocupada mucho los gastos y la logística de venta…</strong></h4>
-                    </div>
-                    <div class="col-md-3 pr-5">
-                        <div class="row mb-3">
-                            <img  style="border-radius: 50%" src="https://picsum.photos/70" alt="img generica">
-                        </div>
-                        <div class="row">
-                            <h5>Broker</h5>
-                        </div>
-                        <div class="row">
-                            <p style="font-weight: 200">Saga Falabella <br>testimonio</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--            <h2>ESTO LO PUEDO ELIMINAR</h2>--}}
+{{--            <div class="col mb-5 mt-5">--}}
+{{--                <div class="row pt-5 pb-5 comment-box">--}}
+{{--                    <div class="col-md-9 pl-5">--}}
+{{--                        <h4><strong style="line-height: 2.3rem">Gracias a totalSubastas.com pudimos vender los activos que teníamos en el almacen, nos preocupada mucho los gastos y la logística de venta…</strong></h4>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-md-3 pr-5">--}}
+{{--                        <div class="row mb-3">--}}
+{{--                            <img  style="border-radius: 50%" src="https://picsum.photos/70" alt="img generica">--}}
+{{--                        </div>--}}
+{{--                        <div class="row">--}}
+{{--                            <h5>Broker</h5>--}}
+{{--                        </div>--}}
+{{--                        <div class="row">--}}
+{{--                            <p style="font-weight: 200">Saga Falabella <br>testimonio</p>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 @endsection
